238bd5ac3bb08758fe6afe46d23497e4f144e2ae
Bryanplane <66257727+Bryanplane@users.noreply.github.com>
2020-06-21 13:55:29 +0800

    Testing inverse

--- cachematrix.R
+++ cachematrix.R
@@ -23,6 +23,27 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      i <- x$getinverse()
+      if(!is.null(i)){
+            message("getting cached data")
+            return(i)
+      }
+      data <- x$get()
+      i <- solve(data, ...)
+      x$setinverse(i)
+      i
 }
+
+
+#TEST
+
+TEST <- matrix(c(2,1,0,6,1,8,2,0), 3,3)
+
+#solvingMatrix
+
+solution <- makeCacheMatrix(TEST)
+cacheSolve(solution)
+
+cacheSolve(solution)
 
 
